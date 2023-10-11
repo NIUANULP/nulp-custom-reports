b266166dfcf95a912df4f41d72158ddad6b9b15a
r0han-9 <146091694+r0han-9@users.noreply.github.com>
2023-10-11 13:36:22 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,29 +1,27 @@
 from cassandra.cluster import Cluster
 from fastapi import Request
-
+from cassandra.policies import RoundRobinPolicy,DCAwareRoundRobinPolicy, ExponentialReconnectionPolicy
 # Loading ENV
 from dotenv import load_dotenv
 import os
-
 
 load_dotenv()
 
 def get_personal_info(user_id):
 
     if user_id:
-        
+
         #1. Loading ENV.
         CLUSTER_LINK = os.environ.get('CLUSTER_LINK')
-        cluster = Cluster([f'{CLUSTER_LINK}'])
-        
+        cluster = Cluster([f'{CLUSTER_LINK}'],connect_timeout=20,load_balancing_policy=RoundRobinPolicy(),reconnection_policy=ExponentialReconnectionPolicy(max_attempts=10,max_delay=20,base_delay=10))
+
         USER_INFO_KEYSPACE = os.environ.get('USER_INFO_KEYSPACE')
         USER_INFO_TABLE = os.environ.get('USER_INFO_TABLE')
 
         session = cluster.connect()
         session.default_timeout = 60
 
-        # 2.Setting Keyspaces.
-
+        #2. Setting Keyspaces.
         session.set_keyspace(USER_INFO_KEYSPACE)
         
         query = f"SELECT firstname, lastname, email, maskedemail, maskedphone, phone FROM {USER_INFO_KEYSPACE}.{USER_INFO_TABLE} WHERE userid = '{user_id}' ALLOW FILTERING;"
@@ -66,7 +64,7 @@ def get_course_name(courseid,batchid):
     else:
         query = f"""SELECT name FROM {COURSE_KEYSPACE}.{COURSE_TABLE} WHERE courseid = '{courseid}' ALLOW FILTERING;"""
     
-    cluster = Cluster([f'{CLUSTER_LINK}'])
+    cluster = Cluster([f'{CLUSTER_LINK}'],connect_timeout=20,load_balancing_policy=RoundRobinPolicy(),reconnection_policy=ExponentialReconnectionPolicy(max_attempts=10,max_delay=20,base_delay=10))
     session = cluster.connect()
     session.default_timeout = 60
 
@@ -104,7 +102,7 @@ def batch_start_date(courseid,batchid):
     COURSE_TABLE = os.environ.get('COURSE_TABLE')
     CLUSTER_LINK = os.environ.get('CLUSTER_LINK')
 
-    cluster = Cluster([f'{CLUSTER_LINK}'])
+    cluster = Cluster([f'{CLUSTER_LINK}'],connect_timeout=20,load_balancing_policy=RoundRobinPolicy(),reconnection_policy=ExponentialReconnectionPolicy(max_attempts=10,max_delay=20,base_delay=10))
     session = cluster.connect()
     session.default_timeout = 60
 
@@ -143,7 +141,7 @@ def batch_end_date(courseid,batchid):
     COURSE_TABLE = os.environ.get('COURSE_TABLE')
     CLUSTER_LINK = os.environ.get('CLUSTER_LINK')
 
-    cluster = Cluster([f'{CLUSTER_LINK}'])
+    cluster = Cluster([f'{CLUSTER_LINK}'],connect_timeout=20,load_balancing_policy=RoundRobinPolicy(),reconnection_policy=ExponentialReconnectionPolicy(max_attempts=10,max_delay=20,base_delay=10))
     session = cluster.connect()
     session.default_timeout = 60
 
@@ -184,6 +182,72 @@ def batch_end_date(courseid,batchid):
     finally:
         session.shutdown()
         cluster.shutdown()
+
+# Get all personal info.
+def get_all_personal_info():
+    
+    #1. Loading ENV.
+    CLUSTER_LINK = os.environ.get('CLUSTER_LINK')
+    cluster = Cluster([f'{CLUSTER_LINK}'],connect_timeout=20,load_balancing_policy=RoundRobinPolicy(),reconnection_policy=ExponentialReconnectionPolicy(max_attempts=10,max_delay=20,base_delay=10))
+
+    USER_INFO_KEYSPACE = os.environ.get('USER_INFO_KEYSPACE')
+    USER_INFO_TABLE = os.environ.get('USER_INFO_TABLE')
+
+    session = cluster.connect()
+    session.default_timeout = 60
+
+    #2. Setting Keyspaces.
+    session.set_keyspace(USER_INFO_KEYSPACE)
+    
+    query = f"SELECT userid, firstname, lastname, email, maskedemail, maskedphone, phone FROM {USER_INFO_KEYSPACE}.{USER_INFO_TABLE} ALLOW FILTERING;"
+    query_result = session.execute(query=query)
+    
+    return query_result
+    
+
+# Get all Batch info.
+def get_all_batch_info(courseid,batchid):
+    
+    #1. Loading ENV.
+    CLUSTER_LINK = os.environ.get('CLUSTER_LINK')
+    cluster = Cluster([f'{CLUSTER_LINK}'],connect_timeout=20,load_balancing_policy=RoundRobinPolicy(),reconnection_policy=ExponentialReconnectionPolicy(max_attempts=10,max_delay=20,base_delay=10))
+
+    COURSE_KEYSPACE = os.environ.get('COURSE_KEYSPACE')
+    COURSE_TABLE = os.environ.get('COURSE_TABLE')
+
+    session = cluster.connect()
+    session.default_timeout = 60
+
+    #2. Setting Keyspaces.
+    session.set_keyspace(COURSE_KEYSPACE)
+    
+    if batchid != '':
+        query = f"SELECT courseid, batchid, name, end_date, start_date FROM {COURSE_KEYSPACE}.{COURSE_TABLE} WHERE courseid = '{courseid}' and batchid = '{batchid}' ALLOW FILTERING;"
+        query_result = session.execute(query=query)
+        
+        return query_result
+    else:
+        query = f"SELECT courseid, batchid, name, end_date, start_date FROM {COURSE_KEYSPACE}.{COURSE_TABLE} WHERE courseid = '{courseid}' ALLOW FILTERING;"
+        query_result_batchinfo = session.execute(query=query)
+        
+        
+        return query_result_batchinfo
+
+    
+
+
+# Search Filter (username).
+
+def filter_query_dict(input_list, search_value):
+    filtered_list = []
+    for item in input_list:
+        for key, value in item.items():
+            if isinstance(value, (str, int)) and isinstance(search_value, (str, int)):
+                if str(search_value).lower() in str(value).lower():
+                    filtered_list.append(item)
+                    break 
+    return filtered_list
+
 
 def temp_response():
     # Temp dummy response.
@@ -254,12 +318,4 @@ def temp_response():
 
     return response
 
-def filter_query_dict(input_list, search_value):
-    filtered_list = []
-    for item in input_list:
-        for key, value in item.items():
-            if isinstance(value, (str, int)) and isinstance(search_value, (str, int)):
-                if str(search_value).lower() in str(value).lower():
-                    filtered_list.append(item)
-                    break  # Break loop if a match is found
-    return filtered_list
+
