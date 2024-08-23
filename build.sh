2d06f2e5b178ea65ffd0abaaeeaf3956d4115ef8
vky25 <93203895+vky25@users.noreply.github.com>
2024-08-23 12:27:12 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Build script
+set -eo pipefail
+
+build_tag=$1
+name=$2
+node=$3
+org=$4
+
+docker build -t ${imageName}:${imageTag} .
+echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
