c47bde605aa627f1dd285c2038206836930e9602
vky25 <93203895+vky25@users.noreply.github.com>
2024-08-23 14:11:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,13 @@ name=$2
 node=$3
 org=$4
 
-docker build -t ${imageName}:${imageTag} .
-echo {\"image_name\" : \"${imageName}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
+# Define image name and tag
+imageName="${org}/${name}"
+imageTag="${build_tag}"
+
+# Build Docker image
+docker build -t "${imageName}:${imageTag}" .
+
+# Create metadata.json with the relevant details
+echo "{\"image_name\" : \"${imageName}\", \"image_tag\" : \"${imageTag}\", \"node_name\" : \"${node}\"}" > metadata.json
+
